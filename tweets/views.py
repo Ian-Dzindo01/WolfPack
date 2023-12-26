05460193eb6008fadf640f7ab8c869c2a89bc3db
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2023-12-26 12:30:52 +0100

    Like implementation

--- tweets/views.py
+++ tweets/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404, JsonResponse
 from .models import Tweet
+import random 
 
 def home_view(request, *args, **kwargs):
-    # return HttpResponse("<h1>Hello World<h1>")
     return render(request, "pages/home.html", context={}, status=200)
 
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{"id":x.id, "content": x.content} for x in qs]
+    tweets_list = [{"id":x.id, "content": x.content, "likes":random.randint(0, 2000)} for x in qs]
     data = {
         "isUser":False,
         "response": tweets_list
@@ -18,11 +18,9 @@ def tweet_list_view(request, *args, **kwargs):
 
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-
     data = {
     "id": tweet_id
     }  
-
     status = 200 
     try:
         obj = Tweet.objects.get(id=tweet_id)
