9257d820619c1bed8763c21112bf7e42e1e3b832
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2023-12-28 13:13:55 +0100

    Stable point

--- tweets/views.py
+++ tweets/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404, JsonResponse
+from django.utils.http import url_has_allowed_host_and_scheme
+from django.conf import settings
+
 from .forms import TweetForm
 from .models import Tweet
 import random 
 
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
     
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
@@ -12,11 +16,15 @@ def home_view(request, *args, **kwargs):
 def tweet_create_view(request, *arg, **kwargs):
     form = TweetForm(request.POST or None)
     next_url = request.POST.get("next") or None
-    
+
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
-        if next_url != None:
+        # if request.is_ajax():                      # ajax is deprecated
+        #     return JsonResponse({}, status=201)    # 201 is for created items
+
+
+        if next_url != None and url_has_allowed_host_and_scheme(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
 
