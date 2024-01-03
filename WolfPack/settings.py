6bdd5f426a5f798033258652456f5b0718338dc7
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2024-01-03 15:01:24 +0100

    Fix register

--- WolfPack/settings.py
+++ WolfPack/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_bootstrap4",
     "tweets",
     "crispy_forms"
 
@@ -115,3 +116,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_REDIRECT_URL = "/"
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
