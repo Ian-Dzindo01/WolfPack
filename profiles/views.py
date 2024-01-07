85041e2fd595c7a1c2940ae56aa1cea28ae28611
Ian-Dzindo01 <ian.dzindo01@gmail.com>
2024-01-07 15:31:37 +0100

    Add check if user exists

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.http import Http404
+
+from .models import Profile
 
 def profile_detailed_view(request, username, *args, **kwargs):
+    qs = Profile.objects.filter(user_username=username):
+    if not qs.exists():
+        raise Http404
+    profile_object = qs.first()
+    context = {
+        "username": username,
+        "profile": profile_obj
+    }
     return render(request, "profiles/detail.html", {"username":username})
